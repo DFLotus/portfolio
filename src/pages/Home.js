23d05282f18437c073551f9fe3ed23485ff0eeec
DFLotus <DEONDRAE.FRANCIS46@myhunter.cuny.edu>
2023-08-30 15:02:34 -0400

    social links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Imgbg from '../assets/home-wallpaper.jpeg'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 import '../styles/Home.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
@@ -10,6 +13,21 @@ const Home = () => {
             </div>
             <div className="my-name">
                 <h1>Deondrae Francis</h1>
+                <div className="social-container">
+                    <ul>
+                        <li><Link></Link></li>
+                        <li>
+                            <a href="https://github.com/DFLotus" target={"_blank"} rel={"noopener noreferrer"}>
+                                <FaGithub className="social-icon" size={"3rem"} color={"white"} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/deondrae-francis-b882241b2/" target={"_blank"} rel={"noopener noreferrer"}>
+                                <FaLinkedin className="social-icon" size={"3rem"} color={"white"} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
