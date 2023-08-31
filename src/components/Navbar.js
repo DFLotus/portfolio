70b4c9dbb1f997c840ccd76630d4d862e8c0b516
DFLotus <deondraefrancis101@gmail.com>
2023-08-30 23:15:29 -0400

    About section photo & bio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 import '../styles/Navbar.css'
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
         <div className="nav-bar-container">
             <nav className="nav-card">
                 <ul className="nav-items">
-                    <li><a>About</a></li>
+                    <li><Link to="about-container" spy={true} smooth={true} duration={100}>About</Link></li>
                     <li><a>Resume</a></li>
                     <li><a>Projects</a></li>
                     <li><a>Affiliations</a></li>
