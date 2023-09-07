20bd4ed7a47642ac7ad64a5c2c7ffec334735a1f
DFLotus <DEONDRAE.FRANCIS46@myhunter.cuny.edu>
2023-09-06 21:25:24 -0400

    Fixed horizontal scrolling for different views

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import './App.css'
 
 function App() {
   return (
